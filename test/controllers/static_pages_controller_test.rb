bdbdc01cb020f8b7d2fa82cf000bbe38c7e2e242
Russell himelein <russ.himelein@gmail.com>
2016-08-14 17:44:22 -0400

    add static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get Q" do
+    get static_pages_Q_url
+    assert_response :success
+  end
+
+end
