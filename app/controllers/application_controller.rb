9b754a383981e5531c3e064cc54c4416ffe41471
Russell himelein <russ.himelein@gmail.com>
2016-08-14 17:42:11 -0400

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+    render html: "This is a site - intuitive/feeling to think"
+  end
 end
